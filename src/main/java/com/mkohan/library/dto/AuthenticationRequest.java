0f3500b673eb5d7392ed8b1fe91abb93429a4547
Max <mkogan58@gmail.com>
2022-08-22 02:06:37 +0300

    Move AuthenticationRequest to Lombok

--- src/main/java/com/mkohan/library/dto/AuthenticationRequest.java
+++ src/main/java/com/mkohan/library/dto/AuthenticationRequest.java
@@ -1,19 +1,15 @@
 package com.mkohan.library.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationRequest {
 
     private String username;
 
     private String password;
-
-    protected AuthenticationRequest() {
-    }
-
-    public AuthenticationRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
--- src/test/java/com/mkohan/library/controller/AuthenticationControllerTest.java
+++ src/test/java/com/mkohan/library/controller/AuthenticationControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.mkohan.library.config.UserDetailsConfiguration.ADMIN_PASSWORD;
 import static com.mkohan.library.config.UserDetailsConfiguration.ADMIN_USERNAME;
